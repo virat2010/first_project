7ebb1a7c56f0fd084902bde6aece6be94fde039d
Virat <59180983+virat2010@users.noreply.github.com>
2022-08-08 14:00:40 -0400

    asd

--- first_project/settings.py
+++ first_project/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 STATIC_DIR = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = os.path.join(STATIC_DIR,"root")
 MEDIA_DIR = os.path.join(BASE_DIR,"media")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
