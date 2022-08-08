66872a297010d8a0cd89ebc78aaac466321ea899
Virat <59180983+virat2010@users.noreply.github.com>
2022-08-08 15:25:35 -0400

    Update urls.py

--- first_project/urls.py
+++ first_project/urls.py
@@ -21,12 +21,12 @@ from django.urls import *
 handler404 = 'first_app.views.error_404'
 
 urlpatterns = [
-    path(r'^$', views.index, name='index'),
-    path(r'^help/', views.help, name='help'),
-    path(r'^users/', views.users, name='users'),
-    path(r'^form/', views.form, name='form'),
-    path(r'^register/', views.register, name='register'),
-    path(r'^login/', views.login, name='login'),
-    path(r'first_app/',include('first_app.urls')),
-    path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^help/', views.help, name='help'),
+    re_path(r'^users/', views.users, name='users'),
+    re_path(r'^form/', views.form, name='form'),
+    re_path(r'^register/', views.register, name='register'),
+    re_path(r'^login/', views.login, name='login'),
+    re_path(r'first_app/',include('first_app.urls')),
+    re_path(r'^admin/', admin.site.urls),
 ]
