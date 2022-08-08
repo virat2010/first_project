147c65883c18d1d86fcfb84029981859394162d4
Virat <59180983+virat2010@users.noreply.github.com>
2022-08-08 15:21:39 -0400

    Update urls.py

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import url
 from first_app import views
 from django.urls import *
 from django.conf.urls import *
 app_name = 'first_app'
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    url(r'^help/',views.help,name='help'),
-    url(r'^users/',views.users,name='users'),
-    url(r'^form/',views.form,name='form'),
-    url(r'^register/', views.register, name='register'),
-    url(r'^login/', views.login, name='login'),
+    path(r'^$',views.index,name='index'),
+    path(r'^help/',views.help,name='help'),
+    path(r'^users/',views.users,name='users'),
+    path(r'^form/',views.form,name='form'),
+    path(r'^register/', views.register, name='register'),
+    path(r'^login/', views.login, name='login'),
 ]
